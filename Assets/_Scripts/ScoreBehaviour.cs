0ed86b5790b6d4f892f6ecd2c3cfbbb8542a20b9
dvyas98 <dhimant92@gmail.com>
2020-11-18 21:25:57 -0500

    Assignment 2 part 1

---
+++ Assets/_Scripts/ScoreBehaviour.cs
@@ -0,0 +1,37 @@
+﻿/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Created BY :
+Dhimant Vyas
+101199558
+Game Programming (T163)
+DVSquareProductions.
+
+Button Behavoiour
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBehaviour : MonoBehaviour
+{
+    public Text Score;
+    public float ScoreAmount;
+    public float Increase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreAmount = 0.0f;
+        Increase = 1.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score.text = (int) ScoreAmount + ".";
+        ScoreAmount += Increase * Time.deltaTime;   // At evry Second add one number to the Counter.
+    }
+}
